5b0acf14ebcd4811e9ca4cc79936192865fd0d0a
syam <kiransyam14@gmail.com>
2026-02-12 18:21:46 +0530

    Milestone 1: Backend setup and MongoDB connection configured

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");  // 👈 must be here
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();  // 👈 must be called
 
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
